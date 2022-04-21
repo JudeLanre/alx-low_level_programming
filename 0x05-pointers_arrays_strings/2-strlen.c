24228d43d9a19dcfd132da2fd57e8a391917894e
Olanrewaju Jude <jeylanre@gmail.com>
2022-04-20 18:47:58 -0700

    h

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,19 +1,16 @@
+#include "main.h"
 
- ​#​include​ ​"​main.h​" 
-  
- ​/*​* 
- ​ * _strlen - find the length of a string 
- ​ * @s: pointer to the string to check 
- ​ * Return: void 
- ​*/ 
-  
-  
- ​int​ ​_strlen​(​char​ *s) 
- ​{ 
- ​        ​int​ i = ​0​; 
-  
- ​        ​while​ (s[i]) 
- ​                i++; 
-  
- ​        ​return​ (i); 
- ​}
+/**
+ * _strlen - returns the length of a string.
+ * @s: input string.
+ * Return: length of a string.
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+	count++;
+	return (count);
+}
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,22 +1,23 @@
- ​#​include​ ​"​main.h​" 
- ​/*​* 
- ​ * print_rev - prints reversed string, followed by a new line 
- ​ * @s: pointer to the string to print 
- ​ * Return: void 
- ​*/ 
-  
- ​void​ ​print_rev​(​char​ *s) 
- ​{ 
- ​        ​int​ i = ​0​; 
-  
- ​        ​while​ (s[i]) 
- ​                i++; 
-  
- ​        ​while​ (i--) 
- ​        { 
- ​                ​_putchar​(s[i]); 
- ​        } 
-  
- ​        ​_putchar​(​'​\n​'​); 
-  
- ​}
+#include "main.h"
+
+/**
+*
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: input string.
+ * Return: no return.
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+			break;
+
+		count++;
+	}
+	for (count--; count >= 0; count--)
+		_putchar(s[count]);
+	_putchar('\n')
+}
