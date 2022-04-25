a8e0c2a8646e2e0c6ae38d9c05310c6b73d48630
Olanrewaju Jude <jeylanre@gmail.com>
2022-04-24 22:00:13 -0700

    f

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,6 +9,7 @@
  */
 
 char *cap_string(char *s)
+
 {
 	int i, j;
 	char delimeters[] = " \t\n,;.!?\"(){}";
@@ -17,9 +18,9 @@ char *cap_string(char *s)
 	{
 		if (s[0] >= 97 && s[0] <= 122)
 			s[0] = s[0] - 32;
-			for (j = 0; delimeters[j] != '\0'; j++)
-				if (s[i] == delimeters[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
-					s[i + 1] = s[i + 1] - 32;
+				for (j = 0; delimeters[j] != '\0'; j++)
+					if (s[i] == delimeters[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
+						s[i + 1] = s[i + 1] - 32;
 	}
 	return (s);
 }
